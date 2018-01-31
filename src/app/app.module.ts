14aab141f14b6662011bb234f4fd3c56c804a6a3
John L Papa <papa@Johns-MBP-2.jetpack>
2018-01-31 14:59:23 -0500

    saved input decorator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroListComponent],
+  declarations: [AppComponent, HeroListComponent, HeroDetailComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
