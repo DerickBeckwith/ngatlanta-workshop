068c7832be26ce9945885de98d177fb1c809d775
John L Papa <papa@Johns-MBP-2.jetpack>
2018-01-31 21:24:53 -0500

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HeroListComponent } from './hero-list/hero-list.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
+import { HomeComponent } from './home/home.component';
+import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, HeroListComponent, HeroDetailComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, HomeComponent, PathNotFoundComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
   providers: [HeroService],
   bootstrap: [AppComponent]
 })
---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+import { HeroesRoutingModule } from './heroes-routing.module';
+
+@NgModule({
+  imports: [CommonModule, HeroesRoutingModule, FormsModule, HttpClientModule, RouterModule],
+  declarations: [HeroListComponent, HeroDetailComponent]
+})
+export class HeroesModule {}
